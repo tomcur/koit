f5673ffa1474b4daa40dee2dcac2f339c75f243a
gcxfd <i@rmw.link>
2021-05-20 12:04:21 +0800

    add toml-format

--- src/format.rs
+++ src/format.rs
@@ -5,71 +5,101 @@
 /// By implementing this trait, a type becomes a marker for the specified format.
 /// That type can then be used for formatting (without instantiating an object of that type).
 pub trait Format<T>: Sized {
-    type Error: std::error::Error + Send + Sync + 'static;
+  type SerError: std::error::Error + Send + Sync + 'static;
+  type DeError: std::error::Error + Send + Sync + 'static;
 
-    /// Convert data to bytes.
-    ///
-    /// # Errors
-    ///
-    /// If the data failed to be encoded by the format, an error variant is returned.
-    fn to_bytes(value: &T) -> Result<Vec<u8>, Self::Error>;
+  /// Convert data to bytes.
+  ///
+  /// # Errors
+  ///
+  /// If the data failed to be encoded by the format, an error variant is returned.
+  fn to_bytes(value: &T) -> Result<Vec<u8>, Self::SerError>;
 
-    /// Convert bytes to data.
-    ///
-    /// # Errors
-    ///
-    /// If the bytes failed to be decoded by the format, an error variant is returned.
-    fn from_bytes(data: Vec<u8>) -> Result<T, Self::Error>;
+  /// Convert bytes to data.
+  ///
+  /// # Errors
+  ///
+  /// If the bytes failed to be decoded by the format, an error variant is returned.
+  fn from_bytes(data: Vec<u8>) -> Result<T, Self::DeError>;
 }
 
 #[cfg(feature = "json-format")]
 pub use self::json::Json;
 
-#[cfg(feature = "bincode-format")]
-pub use self::bincode::Bincode;
-
 #[cfg(feature = "json-format")]
 mod json {
-    use serde::{de::DeserializeOwned, Serialize};
+  use serde::{de::DeserializeOwned, Serialize};
 
-    use super::Format;
+  use super::Format;
 
-    #[cfg_attr(docsrs, doc(cfg(feature = "json-format")))]
-    /// A pretty-printed JSON [`Format`](crate::format::Format).
-    #[derive(Debug, std::default::Default)]
-    pub struct Json;
+  #[cfg_attr(docsrs, doc(cfg(feature = "json-format")))]
+  /// A pretty-printed JSON [`Format`](crate::format::Format).
+  #[derive(Debug, std::default::Default)]
+  pub struct Json;
 
-    impl<T: DeserializeOwned + Serialize> Format<T> for Json {
-        type Error = serde_json::Error;
+  impl<T: DeserializeOwned + Serialize> Format<T> for Json {
+    type DeError = serde_json::Error;
+    type SerError = Self::DeError;
 
-        fn to_bytes(value: &T) -> Result<Vec<u8>, Self::Error> {
-            Ok(serde_json::to_vec_pretty(value)?)
-        }
-        fn from_bytes(data: Vec<u8>) -> Result<T, serde_json::Error> {
-            Ok(serde_json::from_slice(&data)?)
-        }
+    fn to_bytes(value: &T) -> Result<Vec<u8>, Self::SerError> {
+      Ok(serde_json::to_vec_pretty(value)?)
     }
+    fn from_bytes(data: Vec<u8>) -> Result<T, Self::DeError> {
+      Ok(serde_json::from_slice(&data)?)
+    }
+  }
 }
 
 #[cfg(feature = "bincode-format")]
+pub use self::bincode::Bincode;
+
+#[cfg(feature = "bincode-format")]
 mod bincode {
-    use serde::{de::DeserializeOwned, Serialize};
+  use serde::{de::DeserializeOwned, Serialize};
 
-    use super::Format;
+  use super::Format;
 
-    #[cfg_attr(docsrs, doc(cfg(feature = "bincode-format")))]
-    /// A Bincode [`Format`](crate::format::Format).
-    #[derive(Debug, std::default::Default)]
-    pub struct Bincode;
+  #[cfg_attr(docsrs, doc(cfg(feature = "bincode-format")))]
+  /// A Bincode [`Format`](crate::format::Format).
+  #[derive(Debug, std::default::Default)]
+  pub struct Bincode;
 
-    impl<T: Serialize + DeserializeOwned> Format<T> for Bincode {
-        type Error = bincode::Error;
+  impl<T: Serialize + DeserializeOwned> Format<T> for Bincode {
+    type DeError = bincode::Error;
+    type SerError = Self::DeError;
 
-        fn to_bytes(value: &T) -> Result<Vec<u8>, Self::Error> {
-            Ok(bincode::serialize(value)?)
-        }
-        fn from_bytes(data: Vec<u8>) -> Result<T, Self::Error> {
-            Ok(bincode::deserialize(&data)?)
-        }
+    fn to_bytes(value: &T) -> Result<Vec<u8>, Self::SerError> {
+      Ok(bincode::serialize(value)?)
     }
+    fn from_bytes(data: Vec<u8>) -> Result<T, Self::DeError> {
+      Ok(bincode::deserialize(&data)?)
+    }
+  }
+}
+
+#[cfg(feature = "toml-format")]
+pub use self::toml::Toml;
+
+#[cfg(feature = "toml-format")]
+mod toml {
+  use serde::{de::DeserializeOwned, Serialize};
+
+  use super::Format;
+
+  #[cfg_attr(docsrs, doc(cfg(feature = "toml-format")))]
+  /// A pretty-printed Toml [`Format`](crate::format::Format).
+  #[derive(Debug, std::default::Default)]
+  pub struct Toml;
+
+  impl<T: DeserializeOwned + Serialize> Format<T> for Toml {
+    type SerError = toml::ser::Error;
+    type DeError = toml::de::Error;
+
+    fn to_bytes(value: &T) -> Result<Vec<u8>, Self::SerError> {
+      Ok(toml::to_vec(value)?)
+    }
+    fn from_bytes(data: Vec<u8>) -> Result<T, Self::DeError> {
+      Ok(toml::from_slice(&data)?)
+    }
+  }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
 //! ```
 //! use std::default::Default;
 //!
-//! use koit::{FileDatabase, format::Json};
+//! use koit::{FileDatabase, format::Toml};
 //! use serde::{Deserialize, Serialize};
 //!
 //! #[derive(Default, Deserialize, Serialize)]
@@ -16,7 +16,7 @@
 //!
 //! #[async_std::main]
 //! async fn main() -> Result<(), Box<dyn std::error::Error>> {
-//!     let db = FileDatabase::<Data, Json>::load_from_path_or_default("./db.json").await?;
+//!     let db = FileDatabase::<Data, Toml>::load_from_path_or_default("./db.toml").await?;
 //!   
 //!     db.write(|data| {
 //!         data.cats = 10;
@@ -63,179 +63,180 @@ pub use format::Format;
 /// It requires a [`Format`](crate::format::Format) marker type
 #[derive(Debug)]
 pub struct Database<D, B, F> {
-    data: RwLock<D>,
-    backend: Mutex<B>,
-    _format: PhantomData<F>,
+  data: RwLock<D>,
+  backend: Mutex<B>,
+  _format: PhantomData<F>,
 }
 
 impl<D, B, F> Database<D, B, F>
 where
-    B: Backend,
-    F: Format<D>,
+  B: Backend,
+  F: Format<D>,
 {
-    /// Create a database from its constituents.
-    pub fn from_parts(data: D, backend: B) -> Self {
-        Self {
-            data: RwLock::new(data),
-            backend: Mutex::new(backend),
-            _format: PhantomData,
-        }
+  /// Create a database from its constituents.
+  pub fn from_parts(data: D, backend: B) -> Self {
+    Self {
+      data: RwLock::new(data),
+      backend: Mutex::new(backend),
+      _format: PhantomData,
     }
+  }
 
-    /// Write to the data contained in the database.  This gives exclusive access to the underlying
-    /// data structure. The value your closure returns will be passed on as the return value of this
-    /// function.
-    ///
-    /// This write-locks the data structure.
-    pub async fn write<T, R>(&self, task: T) -> R
-    where
-        T: FnOnce(&mut D) -> R,
-    {
-        let mut data = self.data.write().await;
-        task(&mut data)
-    }
+  /// Write to the data contained in the database.  This gives exclusive access to the underlying
+  /// data structure. The value your closure returns will be passed on as the return value of this
+  /// function.
+  ///
+  /// This write-locks the data structure.
+  pub async fn write<T, R>(&self, task: T) -> R
+  where
+    T: FnOnce(&mut D) -> R,
+  {
+    let mut data = self.data.write().await;
+    task(&mut data)
+  }
 
-    /// Same as [`crate::Database::write`], except the task returns a future.
-    pub async fn write_and_then<T, Fut, R>(&self, task: T) -> R
-    where
-        T: FnOnce(&mut D) -> Fut,
-        Fut: Future<Output = R>,
-    {
-        let mut data = self.data.write().await;
-        task(&mut data).await
-    }
+  /// Same as [`crate::Database::write`], except the task returns a future.
+  pub async fn write_and_then<T, Fut, R>(&self, task: T) -> R
+  where
+    T: FnOnce(&mut D) -> Fut,
+    Fut: Future<Output = R>,
+  {
+    let mut data = self.data.write().await;
+    task(&mut data).await
+  }
 
-    /// Read the data contained in the database. Many readers can read in parallel.
-    /// The value your closure returns will be passed on as the return value of this function.
-    ///
-    /// This read-locks the data structure.
-    pub async fn read<T, R>(&self, task: T) -> R
-    where
-        T: FnOnce(&D) -> R,
-    {
-        let data = self.data.read().await;
-        task(&data)
-    }
+  /// Read the data contained in the database. Many readers can read in parallel.
+  /// The value your closure returns will be passed on as the return value of this function.
+  ///
+  /// This read-locks the data structure.
+  pub async fn read<T, R>(&self, task: T) -> R
+  where
+    T: FnOnce(&D) -> R,
+  {
+    let data = self.data.read().await;
+    task(&data)
+  }
 
-    /// Same as [`crate::Database::read`], except the task returns a future.
-    pub async fn read_and_then<T, Fut, R>(&self, task: T) -> R
-    where
-        T: FnOnce(&D) -> Fut,
-        Fut: Future<Output = R>,
-    {
-        let data = self.data.read().await;
-        task(&data).await
-    }
+  /// Same as [`crate::Database::read`], except the task returns a future.
+  pub async fn read_and_then<T, Fut, R>(&self, task: T) -> R
+  where
+    T: FnOnce(&D) -> Fut,
+    Fut: Future<Output = R>,
+  {
+    let data = self.data.read().await;
+    task(&data).await
+  }
 
-    /// Replace the actual data in the database by the given data in the parameter, returning the
-    /// old data.
-    ///
-    /// This write-locks the data structure.
-    pub async fn replace(&self, data: D) -> D {
-        self.write(|actual_data| std::mem::replace(actual_data, data))
-            .await
-    }
+  /// Replace the actual data in the database by the given data in the parameter, returning the
+  /// old data.
+  ///
+  /// This write-locks the data structure.
+  pub async fn replace(&self, data: D) -> D {
+    self
+      .write(|actual_data| std::mem::replace(actual_data, data))
+      .await
+  }
 
-    /// Returns a reference to the underlying data lock.
-    ///
-    /// It is recommended to use the `read` and `write` methods instead of this, to ensure
-    /// locks are only held for as long as needed.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use koit::{Database, format::Json, backend::Memory};
-    ///
-    /// type Messages = Vec<String>;
-    /// let db: Database<_, _, Json> = Database::from_parts(1, Memory::default());
-    ///
-    /// futures::executor::block_on(async move {
-    ///     let data_lock = db.get_data_lock();
-    ///     let mut data = data_lock.write().await;
-    ///     *data = 42;
-    ///     drop(data);
-    ///
-    ///     db.read(|n| assert_eq!(*n, 42)).await;
-    /// });
-    /// ```
-    pub fn get_data_lock(&self) -> &RwLock<D> {
-        &self.data
-    }
+  /// Returns a reference to the underlying data lock.
+  ///
+  /// It is recommended to use the `read` and `write` methods instead of this, to ensure
+  /// locks are only held for as long as needed.
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// use koit::{Database, format::Json, backend::Memory};
+  ///
+  /// type Messages = Vec<String>;
+  /// let db: Database<_, _, Json> = Database::from_parts(1, Memory::default());
+  ///
+  /// futures::executor::block_on(async move {
+  ///     let data_lock = db.get_data_lock();
+  ///     let mut data = data_lock.write().await;
+  ///     *data = 42;
+  ///     drop(data);
+  ///
+  ///     db.read(|n| assert_eq!(*n, 42)).await;
+  /// });
+  /// ```
+  pub fn get_data_lock(&self) -> &RwLock<D> {
+    &self.data
+  }
 
-    /// Returns a mutable reference to the underlying data.
-    ///
-    /// This borrows `Database` mutably; no locking takes place.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use koit::{Database, format::Json, backend::Memory};
-    ///
-    /// let mut db: Database<_, _, Json> = Database::from_parts(1, Memory::default());
-    ///
-    /// let n = db.get_data_mut();
-    /// *n += 41;
-    ///
-    /// futures::executor::block_on(db.read(|n| assert_eq!(*n, 42)));
-    /// ```
-    pub fn get_data_mut(&mut self) -> &mut D {
-        self.data.get_mut()
-    }
+  /// Returns a mutable reference to the underlying data.
+  ///
+  /// This borrows `Database` mutably; no locking takes place.
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// use koit::{Database, format::Json, backend::Memory};
+  ///
+  /// let mut db: Database<_, _, Json> = Database::from_parts(1, Memory::default());
+  ///
+  /// let n = db.get_data_mut();
+  /// *n += 41;
+  ///
+  /// futures::executor::block_on(db.read(|n| assert_eq!(*n, 42)));
+  /// ```
+  pub fn get_data_mut(&mut self) -> &mut D {
+    self.data.get_mut()
+  }
 
-    /// Flush the data contained in the database to the backend.
-    ///
-    /// This read-locks the data structure.
-    ///
-    /// # Errors
-    ///
-    /// - If the data in the database failed to be encoded by the format, an error variant is returned.
-    /// - If the bytes failed to be written to the backend, an error variant is returned. This may mean
-    /// the backend is now corrupted.
-    ///
-    /// # Panics
-    ///
-    /// Some back-ends (such as [`crate::backend::File`]) might panic on some async runtimes.
-    pub async fn save(&self) -> Result<(), KoitError> {
-        let mut backend = self.backend.lock().await;
-        let data = self.data.read().await;
-        backend
-            .write(F::to_bytes(&data).map_err(|err| KoitError::ToFormat(err.into()))?)
-            .await
-            .map_err(|err| KoitError::BackendWrite(err.into()))?;
-        Ok(())
-    }
+  /// Flush the data contained in the database to the backend.
+  ///
+  /// This read-locks the data structure.
+  ///
+  /// # Errors
+  ///
+  /// - If the data in the database failed to be encoded by the format, an error variant is returned.
+  /// - If the bytes failed to be written to the backend, an error variant is returned. This may mean
+  /// the backend is now corrupted.
+  ///
+  /// # Panics
+  ///
+  /// Some back-ends (such as [`crate::backend::File`]) might panic on some async runtimes.
+  pub async fn save(&self) -> Result<(), KoitError> {
+    let mut backend = self.backend.lock().await;
+    let data = self.data.read().await;
+    backend
+      .write(F::to_bytes(&data).map_err(|err| KoitError::ToFormat(err.into()))?)
+      .await
+      .map_err(|err| KoitError::BackendWrite(err.into()))?;
+    Ok(())
+  }
 
-    /// Load data from the backend.
-    async fn load_from_backend(&self) -> Result<D, KoitError> {
-        let mut backend = self.backend.lock().await;
-        let bytes = backend
-            .read()
-            .await
-            .map_err(|err| KoitError::BackendRead(err.into()))?;
-        Ok(F::from_bytes(bytes).map_err(|err| KoitError::FromFormat(err.into()))?)
-    }
+  /// Load data from the backend.
+  async fn load_from_backend(&self) -> Result<D, KoitError> {
+    let mut backend = self.backend.lock().await;
+    let bytes = backend
+      .read()
+      .await
+      .map_err(|err| KoitError::BackendRead(err.into()))?;
+    Ok(F::from_bytes(bytes).map_err(|err| KoitError::FromFormat(err.into()))?)
+  }
 
-    /// Update this database with data from the backend, returning the old data.
-    ///
-    /// This will write-lock the internal data structure.
-    ///
-    /// # Errors
-    ///
-    /// - If the bytes from teh backend failed to be decoded by the format, an error variant is returned.
-    /// - If the bytes failed to be read by the backend, an error variant is returned.
-    ///
-    /// # Panics
-    ///
-    /// Some back-ends (such as [`crate::backend::File`]) might panic on some async runtimes.
-    pub async fn reload(&self) -> Result<D, KoitError> {
-        let new_data = self.load_from_backend().await?;
-        Ok(self.replace(new_data).await)
-    }
+  /// Update this database with data from the backend, returning the old data.
+  ///
+  /// This will write-lock the internal data structure.
+  ///
+  /// # Errors
+  ///
+  /// - If the bytes from teh backend failed to be decoded by the format, an error variant is returned.
+  /// - If the bytes failed to be read by the backend, an error variant is returned.
+  ///
+  /// # Panics
+  ///
+  /// Some back-ends (such as [`crate::backend::File`]) might panic on some async runtimes.
+  pub async fn reload(&self) -> Result<D, KoitError> {
+    let new_data = self.load_from_backend().await?;
+    Ok(self.replace(new_data).await)
+  }
 
-    /// Consume the database and return its data and backend.
-    pub fn into_parts(self) -> (D, B) {
-        (self.data.into_inner(), self.backend.into_inner())
-    }
+  /// Consume the database and return its data and backend.
+  pub fn into_parts(self) -> (D, B) {
+    (self.data.into_inner(), self.backend.into_inner())
+  }
 }
 
 /// A file-backed database.
@@ -248,73 +249,73 @@ pub type FileDatabase<D, F> = Database<D, backend::File, F>;
 #[cfg(feature = "file-backend")]
 impl<D, F> FileDatabase<D, F>
 where
-    F: Format<D>,
+  F: Format<D>,
 {
-    /// Construct the file-backed database from the given path. This attempts to load data
-    /// from the given file.
-    ///
-    /// # Errors
-    /// If the file cannot be read, or the [formatter](crate::format::Format) cannot decode the data,
-    /// an error variant will be returned.
-    pub async fn load_from_path<P>(path: P) -> Result<Self, KoitError>
-    where
-        P: AsRef<std::path::Path>,
-    {
-        let mut backend = backend::File::from_path(path)
-            .await
-            .map_err(|err| KoitError::BackendCreation(err.into()))?;
+  /// Construct the file-backed database from the given path. This attempts to load data
+  /// from the given file.
+  ///
+  /// # Errors
+  /// If the file cannot be read, or the [formatter](crate::format::Format) cannot decode the data,
+  /// an error variant will be returned.
+  pub async fn load_from_path<P>(path: P) -> Result<Self, KoitError>
+  where
+    P: AsRef<std::path::Path>,
+  {
+    let mut backend = backend::File::from_path(path)
+      .await
+      .map_err(|err| KoitError::BackendCreation(err.into()))?;
 
-        let bytes = backend
-            .read()
-            .await
-            .map_err(|err| KoitError::BackendRead(err.into()))?;
-        let data = F::from_bytes(bytes).map_err(|err| KoitError::FromFormat(err.into()))?;
+    let bytes = backend
+      .read()
+      .await
+      .map_err(|err| KoitError::BackendRead(err.into()))?;
+    let data = F::from_bytes(bytes).map_err(|err| KoitError::FromFormat(err.into()))?;
 
-        Ok(Database {
-            data: RwLock::new(data),
-            backend: Mutex::new(backend),
-            _format: PhantomData,
-        })
-    }
+    Ok(Database {
+      data: RwLock::new(data),
+      backend: Mutex::new(backend),
+      _format: PhantomData,
+    })
+  }
 
-    /// Construct the file-backed database from the given path. If the file does not exist,
-    /// the file is created. Then `factory` is called and its return value is used as the initial value.
-    /// This data is immediately and saved to file.
-    pub async fn load_from_path_or_else<P, T>(path: P, factory: T) -> Result<Self, KoitError>
-    where
-        P: AsRef<std::path::Path>,
-        T: FnOnce() -> D,
-    {
-        let (mut backend, exists) = backend::File::from_path_or_create(path)
-            .await
-            .map_err(|e| KoitError::BackendCreation(e.into()))?;
+  /// Construct the file-backed database from the given path. If the file does not exist,
+  /// the file is created. Then `factory` is called and its return value is used as the initial value.
+  /// This data is immediately and saved to file.
+  pub async fn load_from_path_or_else<P, T>(path: P, factory: T) -> Result<Self, KoitError>
+  where
+    P: AsRef<std::path::Path>,
+    T: FnOnce() -> D,
+  {
+    let (mut backend, exists) = backend::File::from_path_or_create(path)
+      .await
+      .map_err(|e| KoitError::BackendCreation(e.into()))?;
 
-        let data = if exists {
-            let bytes = backend
-                .read()
-                .await
-                .map_err(|err| KoitError::BackendRead(err.into()))?;
-            F::from_bytes(bytes).map_err(|err| KoitError::FromFormat(err.into()))?
-        } else {
-            factory()
-        };
+    let data = if exists {
+      let bytes = backend
+        .read()
+        .await
+        .map_err(|err| KoitError::BackendRead(err.into()))?;
+      F::from_bytes(bytes).map_err(|err| KoitError::FromFormat(err.into()))?
+    } else {
+      factory()
+    };
 
-        let db = Database {
-            data: RwLock::new(data),
-            backend: Mutex::new(backend),
-            _format: PhantomData,
-        };
+    let db = Database {
+      data: RwLock::new(data),
+      backend: Mutex::new(backend),
+      _format: PhantomData,
+    };
 
-        db.save().await?;
-        Ok(db)
-    }
+    db.save().await?;
+    Ok(db)
+  }
 
-    /// Same as `load_from_path_or_else`, except it uses [`Default`](`std::default::Default`) instead of a factory.
-    pub async fn load_from_path_or_default<P>(path: P) -> Result<Self, KoitError>
-    where
-        P: AsRef<std::path::Path>,
-        D: std::default::Default,
-    {
-        Self::load_from_path_or_else(path, || std::default::Default::default()).await
-    }
+  /// Same as `load_from_path_or_else`, except it uses [`Default`](`std::default::Default`) instead of a factory.
+  pub async fn load_from_path_or_default<P>(path: P) -> Result<Self, KoitError>
+  where
+    P: AsRef<std::path::Path>,
+    D: std::default::Default,
+  {
+    Self::load_from_path_or_else(path, || std::default::Default::default()).await
+  }
 }
